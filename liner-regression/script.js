37fa1d598e3ae7713f48d6da0e42157dab4e4839
leiquan <leiquan@live.com>
2019-12-25 02:40:06 +0800

    定义模型，代码写完了，但是不能跑

--- liner-regression/script.js
+++ liner-regression/script.js
@@ -2,6 +2,7 @@ import * as tf from "@tensorflow/tfjs";
 import * as tfvis from "@tensorflow/tfjs-vis";
 
 window.onload = () => {
+
     const xs = [1, 2, 3, 4];
     const ys = [1, 3, 5, 7];
     // 散点图，用的时候谷歌找文档
@@ -14,4 +15,10 @@ window.onload = () => {
         xAxisDomain: [0, 5],
         yAxisDomain: [0, 8]
     });
+
+    // why 连续性
+    const model = tf.sequential();
+    // 全链接层
+    model.add(tf.layers.dense({ units: 1, inputShape: [1], }));
+
 }
