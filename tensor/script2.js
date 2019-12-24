3c7634e36612d6b2c0bada42fb5e6eefd00bf9b7
leiquan <leiquan@live.com>
2019-12-25 02:01:13 +0800

    for 循环和TF操作之比较，注意理解

---
+++ tensor/script2.js
@@ -0,0 +1,22 @@
+import * as tf from "@tensorflow/tfjs";
+
+// 传统for循环
+const input = [1, 2, 3, 4];
+const w = [
+    [1, 2, 3, 4],
+    [2, 3, 4, 5],
+    [3, 4, 5, 6],
+    [4, 5, 6, 7]
+];
+const output = [0, 0, 0, 0];
+
+for (let i = 0; i < w.length; i++) {
+    for (let j = 0; j < input.length; j++) {
+        output[i] += input[j] * w[i][j]
+    }
+}
+
+console.log(output);
+
+// tensor 法
+tf.tensor(w).dot(tf.tensor(input)).print();
