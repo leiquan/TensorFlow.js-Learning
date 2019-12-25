d26a7a0ac569ebf52518e8bc85a2209a30fabcdf
leiquan <leiquan@live.com>
2019-12-26 02:41:45 +0800

    完成逻辑梯度下降

--- logistic-regression/script.js
+++ logistic-regression/script.js
@@ -6,20 +6,40 @@ window.onload = async() => {
     const data = getData(400);
     console.log(data);
 
-    tfvis.render.scatterplot({ 'name': '逻辑回归训练数据' }, {
-        values: [
-            data.filter(p => p.label === 1),
-            data.filter(p => p.label === 0)
-        ]
+    tfvis.render.scatterplot({ name: "逻辑回归训练数据" }, {
+        values: [data.filter(p => p.label === 1), data.filter(p => p.label === 0)]
     });
 
     const model = tf.sequential();
-    model.add(tf.layers.dense({
-        units: 1,
-        inputShape: [2], // 注意
-        activation: 'sigmoid' // 将输出值压缩到0-1之间
-    }));
+    model.add(
+        tf.layers.dense({
+            units: 1,
+            inputShape: [2], // 注意
+            activation: "sigmoid" // 将输出值压缩到0-1之间
+        })
+    );
 
-    model.compile({ loss: tf.losses.logLoss });
+    model.compile({ loss: tf.losses.logLoss, optimizer: tf.train.adam(0.1) });
 
+    // 如何把特征数量为2的数据转为tensor
+    const inputs = tf.tensor(data.map(p => [p.x, p.y]));
+    const labels = tf.tensor(data.map(p => p.label));
+
+    await model.fit(inputs, labels, {
+        batchSize: 40,
+        epochs: 50,
+        callbacks: tfvis.show.fitCallbacks({
+            name: "训练过程"
+        }, ['loss'])
+    });
+
+    console.log(model)
+
+    window.predict = (form) => {
+        const pred = model.predict(tf.tensor([
+            [form.x.value * 1, form.y.value * 1]
+        ]))
+
+        alert(`预测结果：${pred.dataSync()[0]}`);
+    }
 };
